e915043e694f99db63d4614851999e6804aace95
changraekang <storkcr@gmail.com>
2021-10-12 18:33:20 +0900

    1. 환경 설정 완료

---
+++ README.md
@@ -0,0 +1,15 @@
+#greenPrject
+
+# 팀원
+-강창래,박용세,이규호
+
+### 개발환경
+-Springboot
+-JPA
+-MariaDB
+-JSP TemplateEngine
+
+
+### DB연결방법
+
+cosdb로 갑니다
---
+++ src/main/java/com/cos/dietApp/test/PageControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.dietApp.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+public class PageControllerTest {
+
+
+	
+}
+
+
